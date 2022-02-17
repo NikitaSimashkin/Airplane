167a199c2775be786c0554c44bc4af78177f0423
NikitaSimashkin <nikita.simashkin@gmail.com>
2022-02-17 23:55:32 +0300

    Гифки v3

--- app/src/main/java/com/example/airplane/MainActivity.java
+++ app/src/main/java/com/example/airplane/MainActivity.java
@@ -17,12 +17,5 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide(); //убирает title_bar
         setContentView(R.layout.activity_main);
 
-       // ImageView main_background = (ImageView) findViewById(R.id.imageView); //загружает гифку
-      //  Glide.with(this).load(R.drawable.krasivo2).into(main_background);
-        /*
-        В папке drawable лежат гифки, можно потом если что поменять
-         */
-
-
     }
 }
