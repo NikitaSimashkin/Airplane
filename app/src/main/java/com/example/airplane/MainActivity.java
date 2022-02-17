ea9f4ad4ed0e938278a3574617361dc20ac53880
NikitaSimashkin <nikita.simashkin@gmail.com>
2022-02-17 14:47:41 +0300

    Очень важное изменение

--- app/src/main/java/com/example/airplane/MainActivity.java
+++ app/src/main/java/com/example/airplane/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        System.out.println("Кирилл пашовкин не умеет программировать");
         Objects.requireNonNull(getSupportActionBar()).hide(); //убирает title_bar
         setContentView(R.layout.activity_main);
     }
