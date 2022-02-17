13c8ac2116f77db85bc56b38e4aaf3df06d695e3
Nikita <123>
2022-02-17 12:26:21 +0300

    2 Убрал лишнее и title bar

--- app/src/main/java/com/example/airplane/MainActivity.java
+++ app/src/main/java/com/example/airplane/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Objects.requireNonNull(getSupportActionBar()).hide(); //убирает title_bar
         setContentView(R.layout.activity_main);
     }
 }
