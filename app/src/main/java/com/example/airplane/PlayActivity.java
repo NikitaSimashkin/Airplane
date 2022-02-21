088ba2045b13e6f5bff44ac18d50281cfe02afdd
NikitaSimashkin <nikita.simashkin@gmail.com>
2022-02-21 23:18:41 +0300

    Сделал кнопки вверх вниз на игровой области

--- app/src/main/java/com/example/airplane/PlayActivity.java
+++ app/src/main/java/com/example/airplane/PlayActivity.java
@@ -1,6 +1,10 @@
 package com.example.airplane;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,24 @@ public class PlayActivity extends AppCompatActivity {
         super.onStart();
         Objects.requireNonNull(getSupportActionBar()).hide(); //убираем title
         setContentView(R.layout.playactivity);
+
+        ImageButton up_button = (ImageButton) findViewById(R.id.up_button);
+        up_button.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "123", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ImageButton down_button = (ImageButton) findViewById(R.id.down_button);
+        down_button.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "456", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
